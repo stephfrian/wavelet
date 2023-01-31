d5f44323d96625dc492a6aa2954db345646d84b5
stephfrian <110694499+stephfrian@users.noreply.github.com>
2023-01-30 18:04:34 -0800

    Create StringServer.java

---
+++ StringServer.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+    String s = new String();
+    String s2 = new String();
+    ArrayList <String> stringArray = new ArrayList<>();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Stephanie's Number: %d", num);
+        } else if (url.getPath().equals("/increment")) {
+            num += 1;
+            return String.format("Number incremented!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add-message")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    s = parameters[1];
+                    stringArray.add(s);
+                    System.out.println(stringArray);
+                    s2 = stringArray.get(0);
+                    s2 = s2 + "\n";
+
+                    for(int i = 1; i<stringArray.size(); i++) {
+                        s2 = s2 + stringArray.get(i);
+                        s2 = s2 + "\n";
+
+                    }
+                    return s2;
+                    //return String.format("%s\n", parameters[1], num);
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+class StringServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
+
+/**
+ * (input)
+    update array
+    print to web server
+ */
